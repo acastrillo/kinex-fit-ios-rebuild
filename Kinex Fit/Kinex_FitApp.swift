a5ce8f822deb6f67b61085ed63de3b75fb9c5bab
Alex C <alejo@cannashieldct.com>
2026-02-20 16:02:49 -0500

    Initial Commit

---
+++ Kinex Fit/Kinex_FitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kinex_FitApp.swift
+//  Kinex Fit
+//
+//  Created by Alex Castrillo on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct Kinex_FitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
